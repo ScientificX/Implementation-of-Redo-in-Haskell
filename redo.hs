6ebd2f3ee7854c0aedbfacd6dfcf911eb6f397fb
favourotiger <favourotiger@gmail.com>
2022-06-06 21:23:37 +0100

    spxr v2

--- redo.hs
+++ redo.hs
@@ -1,7 +1,11 @@
-module Redo where
+module Main where
 
 import qualified System.Process as SP
 import qualified System.Environment as SE
+import System.Exit (ExitCode(..))
+import qualified System.Exit 
+import System.IO 
+import System.Directory
 
 
 
@@ -12,6 +16,10 @@ main = do
 
 redo :: String -> IO ()
 redo file = do
-    (_, _, _, id) <- SP.createProcess $  SP.shell $ "sh " ++ file ++ ".do"
-    _ <- SP.waitForProcess id
-    return ()
+  let tmp = file ++ "--redoing"
+  (_, _, _, id) <- SP.createProcess $  SP.shell $ "sh " ++ file ++ ".do" ++ " - - "++ tmp ++ ">" ++ tmp
+  exit <- SP.waitForProcess id
+  case exit of
+    ExitSuccess -> do renameFile tmp file
+    ExitFailure c -> do hPutStrLn stderr $ "Process rreturned a non zero exit code" ++ show c
+  removeFile tmp
